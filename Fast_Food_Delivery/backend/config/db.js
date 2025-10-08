b4ecbf5ae5cc353803f022c76f094ec0c49fa008
truongnhat140104 <elvis140104@gmail.com>
2025-10-08 11:59:36 +0700

    Admin va GiaiDoan1

--- Fast_Food_Delivery/backend/config/db.js
+++
@@ -1,8 +0,0 @@
-import mongoose from "mongoose";
-
-
-export const connectDB = async () => {
-    // the string which insert in connect function . we can get it in mongodb atlas cloud in 'network access' field.
-    // delete '?' and insert name of project behind the '/'
-    await mongoose.connect('mongodb+srv://elvis140104_db_user:1234566@cluster1.mnnl32w.mongodb.net/food-delivery').then(()=>{console.log("DB connect")})
-}
