c291bec58754ee94b91f211b5dd75d33d9b43143
truongnhat140104 <elvis140104@gmail.com>
2025-10-08 11:48:55 +0700

    Admin va GiaiDoan1

---
+++ Fast_Food_Delivery/admin/src/main.jsx
@@ -0,0 +1,13 @@
+import ReactDom from 'react-dom/client'
+import React from "react" 
+import './index.css'
+import App from './App.jsx'
+import {BrowserRouter} from "react-router-dom"
+
+ReactDom.createRoot(document.getElementById('root')).render(
+
+  <BrowserRouter>
+    <App/>
+  </BrowserRouter>
+
+)
