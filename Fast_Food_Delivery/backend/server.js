70ced6b3ee507a1e57e61e249fe99ec6806f07c1
truongnhat140104 <elvis140104@gmail.com>
2025-10-08 14:45:37 +0700

    Done Giai doan 3 - Cart module

--- Fast_Food_Delivery/backend/server.js
+++ Fast_Food_Delivery/backend/server.js
@@ -3,7 +3,7 @@ import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
-// import cartRouter from "./routes/cartRoute.js"
+import cartRouter from "./routes/cartRoute.js"
 import 'dotenv/config' 
 // import orderRouter from "./routes/orderRoute.js"
 
@@ -24,7 +24,7 @@ connectDB();
 app.use("/api/food",foodRouter)
 app.use('/images', express.static('uploads'))
 app.use("/api/user",userRouter)
-// app.use("/api/cart",cartRouter)
+app.use("/api/cart",cartRouter)
 // app.use("/api/order",orderRouter)
 
 
